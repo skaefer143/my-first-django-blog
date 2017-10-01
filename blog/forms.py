e427fd2a8def68e1f3b6895bc5c69cf02ebcec99
Storm Kaefer <stormkaefer@gmail.com>
2017-10-01 17:30:46 -0600

    Can save new blog posts, if logged in! Also, ignoring PyCharm config files now

--- blog/forms.py
+++ blog/forms.py
@@ -4,6 +4,6 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
 
-	class Meta:
-		model = Post
-		fields = ('title', 'text',)
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
