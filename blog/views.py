cea8b22a9fd471bbca30b1a346639327fc445139
Storm Kaefer <stormkaefer@gmail.com>
2017-10-04 21:43:14 -0600

    finished the forms, added ability to edit posts, and added basic security (if you're an admin)

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,17 @@ from .forms import PostForm
 
 # Create your views here.
 
+
 def post_list(request):
     our_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': our_posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # In case there is no Post with the given pk, it will display the Page Not Found 404 Page.
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -25,4 +28,19 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
